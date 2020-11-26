e7189a3b8475111eea00d37a7ea373489a0bfd74
Murali_Devan <murali@theretailinsights.com>
2020-11-26 07:25:25 +0530

    remove route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Home from './components/Home'
-import Login from './components/Login'
 import searchMovies from './components/Movies'
 import {BrowserRouter as Router,Link,Route} from 'react-router-dom'
 
@@ -11,14 +10,12 @@ function App() {
       <div>
         <nav className="navbar">
           <Link to="/">Home</Link>
-          {/* <Link to="/login">Login</Link> */}
           <Link to="/movies">Movies</Link>
 
 
         </nav>
         <section className="container">
           <Route path="/" component={Home} exact={true}/>
-          {/* <Route path="/login" component={Login} exact={true}/> */}
           <Route path="/movies" component={searchMovies} exact={true}/>
         </section>
     
